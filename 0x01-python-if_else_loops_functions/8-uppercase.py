3fdb0cfbefcaca0f2a1b5f8c6809c8d7d282a92b
Jonnie-Dev <jtech2096@gmail.com>
2024-01-17 03:54:17 +0100

    convert string to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        print("{}".format(ord(char) - 32), end="")
+        print("{}".format(chr(ord(char) - 32)), end="")
     print("{}", end="\n")
