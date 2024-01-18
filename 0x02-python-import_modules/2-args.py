2927a381acc3de796d73a840da13d1a2a91a416d
Jonnie-Dev <jtech2096@gmail.com>
2024-01-18 19:12:21 +0100

    sys module to print arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-import sys
-args = sys.argv[1:]
-args_len = len(args)
-print("{} {}:".format(args_len, "argument" if args_len == 1 else "arguments"))
-for i in  range(args_len):
-    print("{}: {}".format(i+1, args[i]))
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    args_len = len(args)
+    print("{} {}{}".format(args_len, "argument" if args_len == 1 else "arguments", "." if args_len == 0 else ":"))
+    for i in  range(args_len):
+        print("{}: {}".format(i+1, args[i]))
