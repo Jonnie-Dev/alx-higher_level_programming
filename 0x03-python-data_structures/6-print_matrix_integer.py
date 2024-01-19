8f8fdff4d173d34820bd7150ef9f08de258b3b75
Jonnie-Dev <jtech2096@gmail.com>
2024-01-19 22:47:29 +0100

    0x03. Python - Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        arr_i = matrix[i]
+        for j in range(len(arr_i)):
+            end_char = " " if j < len(arr_i) - 1 else "\n"
+            print("{}".format(arr_i[j]), end=end_char)
+    if matrix == [[]]:
+        print()
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Determine the length of the longer list
+    max_length = max(len(tuple_a), len(tuple_b))
+
+    # Pad the shorter list with zeros
+    list_a = list(tuple_a)
+    list_b = list(tuple_b)
+
+    list_a += [0] * (max_length - len(list_a))
+    list_b += [0] * (max_length - len(list_b))
+
+    # Now, both lists have the same length
+    tuple_res = [a + b for a, b in zip(list_a, list_b)]
+    return tuple(tuple_res)
